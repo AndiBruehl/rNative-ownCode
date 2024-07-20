3fa4838f909f5c813e20375783ac279c27d953e9
AndiBruehl <a.bruehl2019@gmail.com>
2024-07-20 12:40:00 +0200

    styling-experiments

--- 03-app/App.js
+++ 03-app/App.js
@@ -1,20 +1,12 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View>
-        <Text
-          style={styles.dummyText}
-        >
-          Another piece of text!
-        </Text>
+        <Text style={styles.dummyText}>Another piece of text!</Text>
       </View>
-      <Text
-        style={styles.dummyText}
-      >
-        Hello World!
-      </Text>
+      <Text style={[styles.dummyText, { color: "red" }]}>Hello World!</Text>
       <Button title="Tap me!" />
     </View>
   );
@@ -23,14 +15,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   dummyText: {
     margin: 16,
     padding: 16,
     borderWidth: 2,
-    borderColor: 'blue',
-  }
+    borderColor: "blue",
+  },
 });
