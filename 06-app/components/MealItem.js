60f3ccbcdd8c6d075665b0a7b2eb1335a8283046
Andreas Brühl <andreas.bruehl@missinggearsolutions.de>
2024-07-22 15:10:49 +0200

    added feedback for buttons

--- 06-app/components/MealItem.js
+++ 06-app/components/MealItem.js
@@ -1,17 +1,33 @@
-import { View, Text, Image, Pressable, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  Pressable,
+  StyleSheet,
+  Platform,
+} from "react-native";
 
 function MealItem({ title, imageUrl, duration, affordability, complexity }) {
   return (
     <View style={[styles.mealItem]}>
-      <Pressable>
-        <View>
-          <Image source={{ uri: imageUrl }} style={[styles.image]} />
-          <Text style={[styles.title]}>{title}</Text>
-        </View>
-        <View>
-          <Text>{duration}min</Text>
-          <Text>{complexity.toUpperCase()}</Text>
-          <Text>{affordability.toUpperCase()}</Text>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+        // onPress={onPress}
+      >
+        <View style={[styles.innerContainer]}>
+          <View>
+            <Image source={{ uri: imageUrl }} style={[styles.image]} />
+            <Text style={[styles.title]}>{title}</Text>
+          </View>
+          <View style={[styles.details]}>
+            <Text style={[styles.text]}>{duration}min</Text>
+            <Text style={[styles.text]}>{complexity.toUpperCase()}</Text>
+            <Text style={[styles.text]}>{affordability.toUpperCase()}</Text>
+          </View>
         </View>
       </Pressable>
     </View>
@@ -26,6 +42,15 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     overflow: "hidden",
     backgroundColor: "#a1a1a1",
+    elevation: 4,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
+  innerContainer: {
+    borderRadius: 8,
   },
   image: {
     width: "100%",
@@ -35,5 +60,21 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     fontSize: 18,
+    margin: 8,
+    // color: "blue",
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+    padding: 8,
+    overflow: "hidden",
+  },
+  text: {
+    // color: "blue",
+    fontStyle: "italic",
+  },
+  buttonPressed: {
+    opacity: 0.5,
   },
 });
