5bcea45ebf012cd9ebde9e8dd99afd6671b5ad23
Andreas Brühl <andreas.bruehl@missinggearsolutions.de>
2024-07-30 17:40:02 +0200

    Adding styling to ManageExpenseScreen

--- 11-app/screens/ManageExpense.js
+++ 11-app/screens/ManageExpense.js
@@ -1,7 +1,51 @@
-import { Text } from "react-native";
+import { useLayoutEffect } from "react";
+import { StyleSheet, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { GlobalStyles } from "../constants/styles";
+import { IconButton } from "../components/UI/IconButton";
 
-function ManageExpense() {
-  return <Text>ManageExpense Screen</Text>;
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+
+  const isEditing = !!editedExpenseId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
+
+  function deleteExpenseHandler() {}
+
+  return (
+    <View style={styles.screenContainer}>
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <Ionicons
+            name="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+  screenContainer: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: GlobalStyles.colors.primary800,
+  },
+  deleteContainer: {
+    marginTop: 16,
+    paddingTop: 8,
+    borderTopWidth: 2,
+    borderTopColor: GlobalStyles.colors.primary200,
+    alignItems: "center",
+  },
+});
