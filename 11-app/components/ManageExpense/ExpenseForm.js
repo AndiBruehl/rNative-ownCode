3f2017dc0b46bbc8667667f328ee44dcb9680e96
Andreas Brühl <andreas.bruehl@missinggearsolutions.de>
2024-07-31 15:39:54 +0200

    further styling

--- 11-app/components/ManageExpense/ExpenseForm.js
+++ 11-app/components/ManageExpense/ExpenseForm.js
@@ -1,26 +1,33 @@
-import { View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import Input from "./Input";
+import { GlobalStyles } from "../../constants/styles";
 
 function ExpenseForm() {
   function amountChangedHandler() {}
   return (
-    <View>
-      <Input
-        label="Amount"
-        textInputConfig={{
-          keyboardType: "decimal-pad",
-          onChangeText: amountChangedHandler,
-        }}
-      />
-      <Input
-        label="Date"
-        textInputConfig={{
-          keyboardType: "number-pad",
-          placeholder: "YYYY-MM-DD",
-          maxLength: 10,
-          onChange: () => {},
-        }}
-      />
+    <View style={styles.form}>
+      <Text style={styles.title}>Your Expense</Text>
+      <View style={styles.inputsRow}>
+        <Input
+          style={styles.rowInput}
+          label="Amount"
+          textInputConfig={{
+            keyboardType: "decimal-pad",
+            onChangeText: amountChangedHandler,
+            textAlign: "right",
+          }}
+        />
+        <Input
+          style={styles.rowInput}
+          label="Date"
+          textInputConfig={{
+            keyboardType: "number-pad",
+            placeholder: "YYYY-MM-DD",
+            maxLength: 10,
+            onChange: () => {},
+          }}
+        />
+      </View>
       <Input
         label="Description"
         textInputConfig={{
@@ -32,3 +39,23 @@ function ExpenseForm() {
 }
 
 export default ExpenseForm;
+
+const styles = StyleSheet.create({
+  form: {
+    marginTop: -10,
+  },
+  title: {
+    fontSize: 24,
+    color: "white", //GlobalStyles.colors.primary100,
+    textAlign: "center",
+    marginVertical: 24,
+    fontWeight: "bold",
+  },
+  inputsRow: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  rowInput: {
+    flex: 1,
+  },
+});
