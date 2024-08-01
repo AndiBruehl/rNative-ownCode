1e0b9f22761d35e13ef4e7b744dba968d468d38d
Andreas Brühl <andreas.bruehl@missinggearsolutions.de>
2024-08-01 18:47:23 +0200

    finished login and signup functions

--- 12-app/screens/LoginScreen.js
+++ 12-app/screens/LoginScreen.js
@@ -1,7 +1,31 @@
-import AuthContent from '../components/Auth/AuthContent';
+import AuthContent from "../components/Auth/AuthContent";
+import { useState } from "react";
+import { login } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  async function loginHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+      await login(email, password);
+    } catch (error) {
+      Alert.alert(
+        "Authentication failed",
+        "Could not create user. Please check your credentials or try again later."
+      );
+      setIsAuthenticating(false);
+      return;
+    }
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message={"Logging in..."} />;
+  }
+
+  return <AuthContent isLogin onAuthenticate={loginHandler} />;
 }
 
 export default LoginScreen;
--- 12-app/screens/SignupScreen.js
+++ 12-app/screens/SignupScreen.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
+import { Alert } from "react-native";
 import AuthContent from "../components/Auth/AuthContent";
 import { createUser } from "../util/auth";
-import LoadingOverlay from "../components/ui/LoadingOverlay"; // Ensure correct import
+import LoadingOverlay from "../components/ui/LoadingOverlay";
 
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
