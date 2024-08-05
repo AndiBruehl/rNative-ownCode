3f63d38f3f55e648021fd2418f6a93e7b8d55750
AndiBruehl <a.bruehl2019@gmail.com>
2024-08-05 23:41:52 +0200

    cleaned location.js

--- 13-app/utils/location.js
+++ 13-app/utils/location.js
@@ -1,11 +1,6 @@
-// utils/location.js
 import { GOOGLE_API_KEY } from "./key";
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
-
-  // Debugging: Print URL
-  console.log("Generated Map Preview URL:", imagePreviewUrl);
-
   return imagePreviewUrl;
 }
