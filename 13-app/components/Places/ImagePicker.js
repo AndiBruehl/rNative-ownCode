07cba5610bb0e452b8e52bcf22412accabf977c0
Andreas Brühl <andreas.bruehl@missinggearsolutions.de>
2024-08-05 16:34:10 +0200

    added preview

--- 13-app/components/Places/ImagePicker.js
+++ 13-app/components/Places/ImagePicker.js
@@ -1,5 +1,5 @@
-import { Alert, Button, View } from "react-native";
-
+import React, { useState } from "react";
+import { Alert, Button, Image, Text, View, StyleSheet } from "react-native";
 import {
   launchCameraAsync,
   PermissionStatus,
@@ -7,13 +7,14 @@ import {
 } from "expo-image-picker";
 
 function ImagePicker() {
+  const [pickedImage, setPickedImage] = useState();
+
   const [cameraPermissionInformation, requestPermission] =
     useCameraPermissions();
 
   async function verifyPermissions() {
     if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
       const permissionResponse = await requestPermission();
-
       return permissionResponse.granted;
     }
 
@@ -35,19 +36,54 @@ function ImagePicker() {
       return;
     }
 
-    const image = await launchCameraAsync({
+    const result = await launchCameraAsync({
       allowsEditing: true,
       aspect: [16, 9],
       quality: 0.5,
     });
-    console.log(image);
+
+    if (!result.canceled && result.assets && result.assets.length > 0) {
+      console.log(result.assets[0].uri);
+      setPickedImage(result.assets[0].uri);
+    } else {
+      console.log("Image picking was canceled or failed");
+    }
   }
+
+  let imagePreview = <Text>No image taken yet.</Text>;
+
+  if (pickedImage) {
+    imagePreview = <Image source={{ uri: pickedImage }} style={styles.image} />;
+  }
+
   return (
-    <View>
-      <View></View>
-      <Button title="take image" onPress={takeImageHandler} />
+    <View style={styles.container}>
+      <View style={styles.imagePreview}>{imagePreview}</View>
+      <Button title="Take Image Now" onPress={takeImageHandler} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 50,
+  },
+  imagePreview: {
+    width: "100%",
+    height: 200,
+    marginBottom: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#ccc",
+    borderWidth: 1,
+    backgroundColor: "#e0e0e0",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
 
 export default ImagePicker;
