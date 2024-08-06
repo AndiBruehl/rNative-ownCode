8d4e5a62b742d055d7b71fa9a1630e2ff231f54d
AndiBruehl <a.bruehl2019@gmail.com>
2024-08-06 21:25:29 +0200

    style bugfix

--- 13-app/App.js
+++ 13-app/App.js
@@ -32,7 +32,7 @@ export default function App() {
 
   return (
     <>
-      <StatusBar style="dark" />
+      <StatusBar style="light" />
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{
