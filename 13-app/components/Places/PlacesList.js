5d7c4d5667d7e8767db0397fb9da194fe8c88599
Andreas Brühl <andreas.bruehl@missinggearsolutions.de>
2024-08-06 15:33:09 +0200

    adding Styling Place Items

--- 13-app/components/Places/PlacesList.js
+++ 13-app/components/Places/PlacesList.js
@@ -17,6 +17,7 @@ function PlacesList({ places }) {
   }
   return (
     <FlatList
+      style={styles.list}
       data={places}
       keyExtractor={(item) => item.id}
       renderItem={({ item }) => <PlaceItem place={item} />}
@@ -27,6 +28,7 @@ function PlacesList({ places }) {
 export default PlacesList;
 
 const styles = StyleSheet.create({
+  list: { margin: 24 },
   fallbackContainer: {
     flex: 1,
     justifyContent: "center",
