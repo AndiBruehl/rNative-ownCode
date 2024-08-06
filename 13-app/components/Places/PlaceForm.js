6ee4f561e00ef938323c00b29dab3d1e04f1fe73
Andreas Brühl <andreas.bruehl@missinggearsolutions.de>
2024-08-06 14:55:46 +0200

    added passing entered data to the "AllPlaces" screen

--- 13-app/components/Places/PlaceForm.js
+++ 13-app/components/Places/PlaceForm.js
@@ -6,7 +6,9 @@ import ImagePicker from "./ImagePicker";
 import LocationPicker from "./LocationPicker";
 import Button from "../ui/Button";
 
-function PlaceForm() {
+import { Place } from "../../models/place";
+
+function PlaceForm({ onCreatePlace }) {
   const [enteredTitle, setEnteredTitle] = useState();
   const [pickedLocation, setPickedLocation] = useState();
   const [selectedImage, setSelectedImage] = useState();
@@ -27,6 +29,9 @@ function PlaceForm() {
     console.log(enteredTitle);
     console.log(selectedImage);
     console.log(pickedLocation);
+
+    const placeData = new Place(enteredTitle, selectedImage, pickedLocation);
+    onCreatePlace(placeData);
   }
 
   return (
