5c26c46d62a9611f42939414a6ce50e8b5c0a7cc
AndiBruehl <a.bruehl2019@gmail.com>
2024-08-06 20:07:30 +0200

    adding database?

--- 13-app/App.js
+++ 13-app/App.js
@@ -1,15 +1,35 @@
+import { useEffect, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import AppLoading from "expo-app-loading";
+
 import AllPlaces from "./screens/AllPlaces";
 import AddPlace from "./screens/AddPlace";
-import Map from "./screens/Map";
 import IconButton from "./components/ui/IconButton";
 import { Colors } from "./constants/colors";
+import Map from "./screens/Map";
+import { init } from "./util/database";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [dbInitialized, setDbInitialized] = useState(false);
+
+  useEffect(() => {
+    init()
+      .then(() => {
+        setDbInitialized(true);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (!dbInitialized) {
+    return <AppLoading />;
+  }
+
   return (
     <>
       <StatusBar style="light" />
@@ -26,12 +46,11 @@ export default function App() {
             component={AllPlaces}
             options={({ navigation }) => ({
               title: "Your Favorite Places",
-              headerTitleAlign: "center",
               headerRight: ({ tintColor }) => (
                 <IconButton
                   icon="add"
-                  color={tintColor}
                   size={24}
+                  color={tintColor}
                   onPress={() => navigation.navigate("AddPlace")}
                 />
               ),
@@ -41,18 +60,10 @@ export default function App() {
             name="AddPlace"
             component={AddPlace}
             options={{
-              headerTitleAlign: "center",
               title: "Add a new Place",
             }}
           />
-          <Stack.Screen
-            name="Map"
-            component={Map}
-            options={{
-              headerTitleAlign: "center",
-              title: "Choose Location",
-            }}
-          />
+          <Stack.Screen name="Map" component={Map} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
