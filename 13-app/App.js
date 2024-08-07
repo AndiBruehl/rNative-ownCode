2174efe6e6297a0c0faf52214f00d62c7dc51b75
Andreas Brühl <andreas.bruehl@missinggearsolutions.de>
2024-08-07 12:28:41 +0200

    light statusbar

--- 13-app/App.js
+++ 13-app/App.js
@@ -33,7 +33,7 @@ export default function App() {
 
   return (
     <>
-      <StatusBar style="dark" />
+      <StatusBar style="light" />
       <NavigationContainer>
         <Stack.Navigator
           screenOptions={{
