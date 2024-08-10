5e2c2f3e3e989a5665f694cbfde0647edef81fd2
AndiBruehl <a.bruehl2019@gmail.com>
2024-08-10 16:31:17 +0200

    deleted key

--- 13-app-eas-build-ready/util/location.js
+++ 13-app-eas-build-ready/util/location.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyCREUxXvXOA7eT4AaX8EkvrqRUaNaS_5Ls";
+const GOOGLE_API_KEY = "your_google_api_key";
 
 export function getMapPreview(lat, lng) {
   const imagePreviewUrl = `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=14&size=400x200&maptype=roadmap&markers=color:red%7Clabel:S%7C${lat},${lng}&key=${GOOGLE_API_KEY}`;
